ff4a24b1b2b12ea8f235878de814d9c8f4356176
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-02-08 20:48:50 +0530

    Created Upload Images and view of the post

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import Profile from "./components/Profile";
+import theme from "./config/theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="2xl" m="3rem auto" p={5} maxW={700}>
+        <Profile />
+      </Box>
+    </ChakraProvider>
   );
 }
 
